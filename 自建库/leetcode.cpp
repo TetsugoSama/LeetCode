5f71578460229b56df568299351172d7a7d1bf9a
TetsugoSama <60079155+TetsugoSama@users.noreply.github.com>
2020-06-16 20:21:39 +0800

    Revert "Update leetcode.cpp"
    This reverts commit c9e4d07bdfedd88461bf6511886261e53586e2aa.

--- 自建库/leetcode.cpp
+++ 自建库/leetcode.cpp
@@ -49,7 +49,6 @@ TreeNode* creatTreeNode(int n)
 TreeNode* creatTree(vector<int>& v)
 {
     vector<TreeNode*> vt(v.size());
-    int amend = 0;
     for(int i=0;i<v.size();i++)
     {
         vt[i] = creatTreeNode(v[i]);
@@ -57,12 +56,9 @@ TreeNode* creatTree(vector<int>& v)
     for(int i=0;i<vt.size();i++)
     {
         if (vt[i] == nullptr)
-        {
-            amend += 2;                 //如果某节点为空，则该节点无子节点，所以不会在数组中出现
             continue;
-        }
-        int left = (i+1)*2-1-amend;  //修正因为空节点导致后面节点的子节点索引错误
-        int right = (i+1)*2-amend;
+        int left = (i+1)*2-1;
+        int right = (i+1)*2;
         if(left>=vt.size())
         {
             vt[i]->left=nullptr;
